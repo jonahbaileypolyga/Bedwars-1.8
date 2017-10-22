a1de2c6cf5f628282010fcf6f1f3d8df12a53f33
byComannds <32988551+byCommands@users.noreply.github.com>
2017-10-22 02:21:28 +0200

    Color Update

--- src/me/longhornhdtv/bedwars/utils/Teams.java
+++ src/me/longhornhdtv/bedwars/utils/Teams.java
@@ -3,12 +3,19 @@ package me.longhornhdtv.bedwars.utils;
 import org.bukkit.Material;
 
 public enum Teams {
-	WHITE(Material.WOOL, 0, "WEISS"),
-	ORANGE(Material.WOOL, 1, "ORANGE"),
-	MAGENTA(Material.WOOL, 2, "MAGENTA"),
-	LIGHTBLUE(Material.WOOL, 3, "HELL BLAU");
+	BLACK(Material.WOOL, 15, "§0SCHWARTZ"),
+	WHITE(Material.WOOL, 0, "§fWEISS"),
+	RED(Material.WOOL, 14, "§cROT"),
+	YELLOW(Material.WOOL, 4, "§eGELB"),
+	GREEN(Material.WOOL, 5, "§aGRÜN"),
+	PURPLE(Material.WOOL, 10, "§5LILA"),
+	ORANGE(Material.WOOL, 1, "§6ORANGE"),
+	BLUE(Material.WOOL, 11, "§9BLAU"),
 	
-	
+	//8x1 Schwartz Weiß Rot Gelb Grün Lila Orange Blau
+	//2x4 BLau Gelb Rot Lila
+	//4x4 BLau Gelb Rot Lila
+	//4x8 BLau Gelb Rot Lila
 	
 	private Material mat;
 	private int metaID;
